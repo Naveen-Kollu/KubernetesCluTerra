28da56ac18fb9fc77aae3fe77a46d9d256e84b68
Naveen-Kollu <62886172+Naveen-Kollu@users.noreply.github.com>
2023-06-16 22:32:55 +0200

    Update main.tf

--- CreateKubernetesCluster/main.tf
+++ CreateKubernetesCluster/main.tf
@@ -10,19 +10,3 @@ resource "azurerm_virtual_network" "KubernetesVnet" {
   resource_group_name = var.resource_group_name
   address_space       = ["12.0.0.0/16"]
 }
-
-resource "azurerm_subnet" "KubernetesSubnet" {
-  name                 = var.azzuremission_team2_subnet_deployagent
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.KubernetesVnet.name
-  address_prefixes     = ["12.0.2.0/24"]
-
-  delegation {
-    name = "Microsoft.Web.hostingEnvironments"
-    service_delegation {
-      name    = "Microsoft.Web/hostingEnvironments"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
-}
-
