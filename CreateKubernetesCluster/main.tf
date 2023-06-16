8852319ed7dd92ef361b5fba6dca92ba25c9c0d5
Naveen-Kollu <62886172+Naveen-Kollu@users.noreply.github.com>
2023-06-16 22:28:33 +0200

    Update main.tf

--- CreateKubernetesCluster/main.tf
+++ CreateKubernetesCluster/main.tf
@@ -26,38 +26,3 @@ resource "azurerm_subnet" "KubernetesSubnet" {
   }
 }
 
-resource "azurerm_kubernetes_cluster" "Team2KubCluName" {
-  name                = "Team2KubCluName"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  dns_prefix          = "my-aks-cluster"
-
-  default_node_pool {
-    name                  = "default"
-    vm_size               = "Standard_DS2_v2"
-    enable_auto_scaling   = true
-    min_count             = 2
-    max_count             = 5
-    enable_node_public_ip = false
-    os_disk_size_gb       = 30
-    
-
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  network_profile {
-    network_plugin    = "kubenet"
-    network_policy    = "calico"
-    load_balancer_sku = "standard"
-  }
-
-  tags = {
-    Environment = "Dev/Test"
-  }
-
-  sku_tier           = "Free"
-  kubernetes_version = "1.26.0"
-}
